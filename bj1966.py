2a66471f252c68f35fdcc7ff867da21b8652f409
eunji-woo <eunji20392@naver.com>
2023-04-24 01:54:18 +0900

    구현 방법 생각,,

--- bj1966.py
+++ bj1966.py
@@ -1,25 +1,32 @@
-from collections import deque
+# from collections import deque
+#
+# count = int(input())
+#
+# for _ in range(count):
+#     n, m = map(int, input().split())
+#     for _ in range(n):
+#         count = 0
+#         important = deque(list(map(int, input().split())))
+#         target = important[m]
+#         while important:
+#             if len(important) == 1:
+#                 print(count + 1)
+#                 break
+#             while important[0] != max(important):
+#                 important.append(important.popleft())
+#                 print(important)
+#             count += 1
+#             if important.popleft() == target:
+#                 print(count)
+#                 break
+#             else:
+#                 continue
+#
+#
 
-count = int(input())
+idx = list(range(3))
+print(idx)
+# [0, 1, 2]
 
-for _ in range(count):
-    n, m = map(int, input().split())
-    for _ in range(n):
-        count = 0
-        important = deque(list(map(int, input().split())))
-        target = important[m]
-        print(target)
-        while important:
-            if len(important) == 1:
-                print(count + 1)
-                break
-            while important[0] != max(important):
-                important.append(important.popleft())
-                print(important)
-            if important.popleft() == target:
-                print(count)
-                break
-            else:
-                count += 1
-                print("카운트 증ㅈ가")
-
+# https://assaeunji.github.io/python/2020-05-04-bj1966/ 참고
+# 리스트 두개로 딕셔너리 느낌을,, 구현한 것 같다고 느꼈다.
