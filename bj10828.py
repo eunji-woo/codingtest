13faef586f6143b9a1c4508c3b33a71b1cd23c1d
eunji-woo <68248243+eunji-woo@users.noreply.github.com>
2023-05-09 17:01:10 +0900

    스택 문제, 반복문으로 입력 반복해서 받을때는 input말고 readline 쓰기!!!!!!!111

--- bj10828.py
+++ bj10828.py
@@ -1,10 +1,28 @@
-# n = int(input())
-#
-# for _ in range(n):
-#     command = input()
-#
+import sys
 
-a = "eunji"
-print(a.split(" ")[0])
-b = "eunji 2"
-print(b.split()[1])
+n = int(input())
+s = list()
+
+for _ in range(n):
+    command = sys.stdin.readline()
+    c = command.split()[0]
+
+    if c == "push":
+        s.append(command.split()[1])
+    elif c == "pop":
+        if len(s) > 0:
+            print(s.pop())
+        else:
+            print("-1")
+    elif c == "size":
+        print(len(s))
+    elif c == "empty":
+        if len(s) == 0:
+            print("1")
+        else:
+            print("0")
+    elif c == "top":
+        if len(s) == 0:
+            print("-1")
+        else:
+            print(s[-1])
